73d65bdce22a8b9e398a3ccb7ad7f440da415f5e
wrecklessCoder <42796011+wrecklessCoder@users.noreply.github.com>
2018-09-12 02:58:53 -0400

    Add files via upload

---
+++ js/headerfooter.js
@@ -0,0 +1,14 @@
+'use strict'
+function $(a){return document.querySelector(a);}function $$(a){return document.querySelectorAll(a);}
+function _(){
+	const h = '<img id="logo" src="Images/LogoGrey.svg"/>' +
+			  '<div id="menu">' +
+			  '<a href="onlinegames.html"><div>Web</div></a>' +
+		      '<a href="mobile.html"><div>Mobile</div></a>' +
+		      '<a href="about.html"><div>About</div></a>' +
+	          '</div>';
+	$('#header').innerHTML = h;
+	const f = '&copy; Copyright 2018 High Stakes Games, LLC. Use of this site constitutes acceptance of the&nbsp;' +
+			  '<a id="Tos" href="pages/termsofservice.html">Terms of Service</a>';
+	$('#footer').innerHTML = f;
+}
