3335f21d615130bcb62fcde7b9c0bac4298b6da9
wrecklessCoder <42796011+wrecklessCoder@users.noreply.github.com>
2018-09-14 04:58:47 -0400

    Update headerfooter.js

--- js/headerfooter.js
+++ js/headerfooter.js
@@ -1,11 +1,20 @@
 'use strict'
 function $(a){return document.querySelector(a);}function $$(a){return document.querySelectorAll(a);}
 function _(){
-	const h = '<a href="index.html"><img id="logo" src="Images/LogoGrey.svg"/></a>' +
+	const h = '<div class="stanDisp">' +
+			  '<div id="headerBody">' +
+			  '<a href="index.html"><img id="logo" src="Images/LogoGrey.svg"/></a>' +
 			  '<div id="menu">' +
 			  '<a href="onlinegames.html"><div>Web</div></a>' +
 		      '<a href="mobile.html"><div>Mobile</div></a>' +
 		      '<a href="about.html"><div>About</div></a>' +
+	          '</div>' +
+	          '</div>' +
+	          '<div id="hBack"></div>' +
+	          '</div>' +
+	          '<div class="respDisp">' +
+			  '<a href="index.html"><img id="centerlogo" src="Images/LogoGrey.svg"/></a>' +
+	          '</div>' +
 	          '</div>';
 	$('#header').innerHTML = h;
 	const f = '&copy; Copyright 2018 High Stakes Games, LLC. Use of this site constitutes acceptance of the&nbsp;' +
