0d95f360f9e805ea5f3c10711fb948bec40f34cf
AceJJ <42796011+AceJJ@users.noreply.github.com>
2018-09-26 22:02:58 -0400

    Update headerfooter.js

--- js/headerfooter.js
+++ js/headerfooter.js
@@ -1,4 +1,3 @@
-'use strict'
 function $(a){if(a.substr(0,1) == '#')return document.getElementById(a.substr(1,a.length));else return document.querySelectorAll(a);}
 function _(scr){
 	var h='<div class="stanDisp"><div id="headerBody"><a href="index.html"><img id="logo" src="Images/LogoGrey.svg"/></a><div id="menu"><a href="mobilegames.html"><div id="menuMobile">Games</div></a><a href="about.html"><div id="menuAbout">About</div></a><a href="support.html"><div id="menuSupport">Support</div></a></div></div><div id="hBack"></div></div><div class="respDisp"><div id="hamMenu"><div class="ham"></div><div class="ham"></div><div class="ham"></div></div><a href="index.html"><img id="centerlogo" src="Images/LogoGrey.svg"/></a><div id ="hamDropDown"><a href="index.html"><div>Home</div></a><a href="mobilegames.html"><div>Games</div></a><a href="about.html"><div>About</div></a><a href="support.html"><div>Support</div></a></div></div></div>';
